8de7e7316042707a425f512dc4e7219cb9762ed2
Alexandra Di Giulian <andigiulian@gmail.com>
2018-05-31 12:01:56 -0400

    Alter playRound and add compareChoice

--- script.js
+++ script.js
@@ -17,27 +17,67 @@ function computerPlay() {
 var computerSelection = computerPlay();
 var playerSelection = prompt("Rock, Paper or Scissors?");
 
-function playRound(playerSelection, computerSelection) {
-	if (playerSelection.toLowerCase() === computerSelection) {
-		return "Draw"
-	} else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-		return "You lose! Paper beats rock!"
-	} else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-		return "You lose! Scissors beats paper!"
-	} else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-		return "You lose! Rock beats Scissors!"
-	} else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-		return "You win! Rock beats Scissors!"
-	} else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-		return "You win! Paper beats rock!"
-	} else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-		return "You win! Scissors beats paper!"
+function compareChoice(playerSelection, computerSelection) {
+	if (playerSelection.toLowerCase() === "rock"){
+		switch (computerSelection){
+			case "rock":
+				return "Draw!";
+				break;
+			case "paper":
+				return "You lose! Paper beats rock!";
+				break;
+			case "scissors":
+				return "You win! Rock beats Scissors!";
+				break;
+		}
+	} else if (playerSelection.toLowerCase() === "paper") {
+		switch (computerSelection) {
+			case "rock":
+				return "You win! Paper beats rock!";
+				break;
+			case "paper":
+				return "Draw!";
+				break;
+			case "scissors":
+				return "You lose! Scissors beats paper!";
+				break;
+		}
+	} else if (playerSelection.toLowerCase() === "scissors") {
+		switch (computerSelection) {
+			case "rock":
+				return "You lose! Rock beats Scissors!";
+				break;
+			case "paper":
+				return "You win! Scissors beats paper!";
+				break;
+			case "scissors":
+				return "Draw!";
+				break;
+		}
 	}
 }
+
+function playRound(playerSelection, computerSelection) {
+	var result = compareChoice(playerSelection, computerSelection);
+	return result;
+}
+
+/*function countScore(result, playerWins, computerWins) {
+	var score = {};
+	if (result.includes("win")) {
+		playerWins++;
+	} else if (result.includes("lose")) {
+		computerWins++;
+	}
+}*/
 
 //console.log(playRound(playerSelection, computerSelection));
 
 
-function game() {
-	console.log(playRound(playerSelection, computerSelection));
+/*function game(num) {
+	for (i = 0; i < num; i++) {
+		playRound(playerSelection, computerSelection);
+	}	
 }
+
+game(5);*/
